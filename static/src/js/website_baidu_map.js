4a525952016dea9d49fcbbfc03eba8dc09e02fae
btredback <564277411@qq.com>
2017-06-17 12:59:11 +0800

    modified

--- static/src/js/website_baidu_map.js
+++ static/src/js/website_baidu_map.js
@@ -2,7 +2,7 @@ function initialize_map() {
     console.log("imap:" + testValue);
     var mapVal = "testValue of Map";
     //$(window.parent.document).find("div.mapVal").text("1223");
-    window.parent.document.getElementById("mapVal").innerHTML = mapVal;
+    //window.parent.document.getElementById("mapVal").innerHTML = mapVal;
     var map = new BMap.Map("odoo-baidu-map");
     var point = new BMap.Point(116.404, 39.915);
     map.centerAndZoom(point, 15)
